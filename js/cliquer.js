821ce8cb636b1a21980e25b48b74efde0f722b57
Juan-Andrey <juanandrey2007@hotmail.com>
2020-12-22 20:46:55 -0300

    add local storage
    salvar cliques no browser

--- js/cliquer.js
+++ js/cliquer.js
@@ -1,5 +1,20 @@
-var clicks = 0;
-function onClick() {
-    clicks += 1;
-    document.getElementById("clicks").innerHTML = clicks;
-};
+function clickCounter() {
+	if (typeof(Storage) !== "undefined") {
+    if (localStorage.clickcount) {
+      localStorage.clickcount = Number(localStorage.clickcount)+1;
+    } else {
+      localStorage.clickcount = 1;
+    }
+    document.getElementById("clicks").innerHTML = localStorage.clickcount;
+  	} else {
+    document.getElementById("clicks").innerHTML = "n suportado";
+  	}
+}
+
+function getClicks() {
+	if (localStorage.clickcount) {
+    document.getElementById("clicks").innerHTML = localStorage.clickcount;
+  	} else {
+    document.getElementById("clicks").innerHTML = 0;
+  	}
+}
