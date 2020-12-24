df721761999113d9c6cc0f6ee86045e02bbdb980
Juan-Andrey <juanandrey2007@hotmail.com>
2020-12-24 02:44:36 -0300

    autopogs

--- js/cliquer.js
+++ js/cliquer.js
@@ -1,20 +1,57 @@
-function clickCounter() {
-	if (typeof(Storage) !== "undefined") {
-    if (localStorage.clickcount) {
-      localStorage.clickcount = Number(localStorage.clickcount)+1;
-    } else {
-      localStorage.clickcount = 1;
-    }
-    document.getElementById("clicks").innerHTML = localStorage.clickcount;
+function getAll() {
+	if (localStorage.pog) {
+    document.getElementById("Poggers").innerHTML = localStorage.pog;
+    pogs = Number(localStorage.pog);
   	} else {
-    document.getElementById("clicks").innerHTML = "n suportado";
+    document.getElementById("Poggers").innerHTML = 0;
   	}
+  if (localStorage.autopog) {
+    document.getElementById("autoPoggers").innerHTML = localStorage.autopog;
+    autopogs = Number(localStorage.autopog);
+    } else {
+    document.getElementById("autoPoggers").innerHTML = 0;
+  }
 }
 
-function getClicks() {
-	if (localStorage.clickcount) {
-    document.getElementById("clicks").innerHTML = localStorage.clickcount;
-  	} else {
-    document.getElementById("clicks").innerHTML = 0;
-  	}
+function pogCounter() {
+  pogs += 1;
+  document.getElementById("Poggers").innerHTML = pogs;
 }
+
+function autoPog() {
+  if (pogs >= 10) {
+    pogs -= 10;
+    autopogs += 1;
+    document.getElementById("autoPoggers").innerHTML = autopogs;
+  }
+}
+
+function incrementPog() {
+  pogs += autopogs;
+  document.getElementById("Poggers").innerHTML = pogs;
+}
+
+function saveGame() {
+  localStorage.pog = pogs;
+  localStorage.autopog = autopogs;
+}
+
+function teste() {
+  localStorage.pog = 0;
+  localStorage.autopog = 0;
+  pogs = 0;
+  autopogs = 0;
+  document.getElementById("autoPoggers").innerHTML = localStorage.autopog;
+  document.getElementById("Poggers").innerHTML = localStorage.pog;
+}
+
+function resetAll() {
+  localStorage.clear();
+}
+
+pogs = 0
+autopogs = 0
+
+setInterval(saveGame, 30000);
+
+setInterval(incrementPog, 1000);
